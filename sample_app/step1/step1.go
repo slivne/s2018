a6f7fee98eac8c9da7e5b6579ad0a49818b73095
Henrik Johansson <dahankzter@gmail.com>
2018-11-02 20:57:50 +0100

    step1: go lint

--- sample_app/step1/step1.go
+++ sample_app/step1/step1.go
@@ -16,13 +16,13 @@ var (
 	users = make([]string, 1000)
 )
 
-func generate_users() {
+func generateUsers() {
 	for i := 0; i < len(users); i++ {
 		users[i] = fmt.Sprintf("user%d", i)
 	}
 }
 
-func get_followers(user string) []string {
+func getFollowers(user string) []string {
 	var id int
 	fmt.Sscanf(user, "user%d", &id)
 	followers := make([]string, 5)
@@ -32,31 +32,31 @@ func get_followers(user string) []string {
 	return followers
 }
 
-func insert_tweet(session *gocql.Session, user string, tweet_id gocql.UUID, tweet_time gocql.UUID, tweet_txt string) {
+func insertTweet(session *gocql.Session, user string, tweetID gocql.UUID, tweetTime gocql.UUID, tweetTxt string) {
 	if err := session.Query(fmt.Sprintf("INSERT INTO tweets (user, tweet_id, time, text) VALUES ('%s',%s, %s,'%s')",
-		user, tweet_id, tweet_time, tweet_txt)).Exec(); err != nil {
+		user, tweetID, tweetTime, tweetTxt)).Exec(); err != nil {
 		log.Fatal(err)
 	}
 
-	for _, follower := range get_followers(user) {
+	for _, follower := range getFollowers(user) {
 		liked := false
 		if rand.Intn(100) < 5 {
 			liked = true
 		}
 		if err := session.Query(fmt.Sprintf("INSERT INTO timeline (user, tweet_id, time, author, text, liked) VALUES ('%s',%s, %s,'%s','%s', %t)",
-			follower, tweet_id, tweet_time, user, tweet_txt, liked)).Exec(); err != nil {
+			follower, tweetID, tweetTime, user, tweetTxt, liked)).Exec(); err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func get_timeline(session *gocql.Session, user string) {
-	var tweet_id gocql.UUID
+func getTimeline(session *gocql.Session, user string) {
+	var tweetID gocql.UUID
 	var author string
 	var text string
 
 	iter := session.Query(fmt.Sprintf("SELECT tweet_id, author, text FROM timeline WHERE user = '%s'", user)).Iter()
-	for iter.Scan(&tweet_id, &author, &text) {
+	for iter.Scan(&tweetID, &author, &text) {
 	}
 	if err := iter.Close(); err != nil {
 		log.Fatal(err)
@@ -79,7 +79,7 @@ func main() {
 	session, _ := cluster.CreateSession()
 	defer session.Close()
 
-	generate_users()
+	generateUsers()
 
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 
@@ -90,10 +90,10 @@ func main() {
 		user := users[random.Intn(len(users))]
 		if random.Intn(10) > 5 {
 			for msg := 0; msg < 100; msg++ {
-				insert_tweet(session, user, gocql.TimeUUID(), gocql.TimeUUID(), fmt.Sprintf("msg_%s_%d", user, msg))
+				insertTweet(session, user, gocql.TimeUUID(), gocql.TimeUUID(), fmt.Sprintf("msg_%s_%d", user, msg))
 			}
 		} else {
-			get_timeline(session, user)
+			getTimeline(session, user)
 		}
 	}
 }
